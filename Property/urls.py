c7adbd63ec86f71aa1852e1701b60fc0e420043e
Arif Shariar Rahman <43639863+ArifShariar@users.noreply.github.com>
2022-02-16 02:00:24 +0600

    Added dynamic urls to hosting pages

--- Property/urls.py
+++ Property/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     # TODO: PASS USER ID IN THE URLS TO MAKE DYNAMIC URL ROUTING
-    path('host/', views.host, name='host'),
-    path('host/success', views.hosting_successful, name='hosting_successful'),
+    path('<str:pk>/host', views.host, name='host'),
+    path('<str:pk>/host/success', views.hosting_successful, name='hosting_successful'),
 
-    path('host/personal', views.host_personal_room, name='host_personal_room'),
-    path('host/business_storage', views.host_business_storage, name='host_business_storage'),
-    path('host/climate_controlled_storage', views.host_climate_controlled_storage,
+    path('<str:pk>/host/personal', views.host_personal_room, name='host_personal_room'),
+    path('<str:pk>/host/business_storage', views.host_business_storage, name='host_business_storage'),
+    path('<str:pk>/host/climate_controlled_storage', views.host_climate_controlled_storage,
          name='host_climate_controlled_storage'),
-    path('host/garage', views.host_garage, name='host_garage'),
+    path('<str:pk>/host/garage', views.host_garage, name='host_garage'),
 ]
--- Property/views.py
+++ Property/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render, redirect
+from django.forms import inlineformset_factory
+from User.models import User
 from .forms import *
 
 
 # Create your views here.
 
 
-def host(request):
+def host(request, pk):
+    user = User.objects.get(pk=pk)
+    if user:
+        print(user.name)
     context = {}
     return render(request, 'host/host.html', context)
 
 
-def hosting_successful(request):
-    return render(request, 'host/hosting_success.html')
+def hosting_successful(request, pk):
+    user = User.objects.get(pk=pk)
+    context = {}
+    if user:
+        context = {'user': user.name}
+
+    return render(request, 'host/hosting_success.html', context)
 
 
-def host_personal_room(request):
-    form = HostPersonalRoomForm()
+def host_personal_room(request, pk):
+    user = User.objects.get(pk=pk)
+    form = HostPersonalRoomForm(initial={'user_id': user})
     context = {'form': form}
 
     if request.method == 'POST':
@@ -33,8 +44,9 @@ def host_personal_room(request):
     return render(request, 'host/personal_room_host.html', context)
 
 
-def host_business_storage(request):
-    form = HostBusinessStorageForm()
+def host_business_storage(request, pk):
+    user = User.objects.get(pk=pk)
+    form = HostBusinessStorageForm(initial={'user_id': user})
     context = {'form': form}
 
     if request.method == 'POST':
@@ -45,8 +57,9 @@ def host_business_storage(request):
     return render(request, 'host/business_storage_host.html', context)
 
 
-def host_climate_controlled_storage(request):
-    form = HostClimateControlledStorageForm()
+def host_climate_controlled_storage(request, pk):
+    user = User.objects.get(pk=pk)
+    form = HostClimateControlledStorageForm(initial={'user_id': user})
     context = {'form': form}
 
     if request.method == 'POST':
@@ -57,8 +70,9 @@ def host_climate_controlled_storage(request):
     return render(request, 'host/climate_controlled_storage_host.html', context)
 
 
-def host_garage(request):
-    form = HostGarageForm()
+def host_garage(request, pk):
+    user = User.objects.get(pk=pk)
+    form = HostGarageForm(initial={'user_id': user})
     context = {'form': form}
 
     if request.method == 'POST':
